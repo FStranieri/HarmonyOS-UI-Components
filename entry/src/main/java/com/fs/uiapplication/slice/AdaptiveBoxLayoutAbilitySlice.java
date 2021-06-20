2d28d07382cab07fd008955bd2050103615e800c
fStrangerZ <fer.stranieri@gmail.com>
2021-06-20 17:57:27 +0200

    adaptiveboxlayout

--- entry/src/main/java/com/fs/uiapplication/slice/AdaptiveBoxLayoutAbilitySlice.java
+++ entry/src/main/java/com/fs/uiapplication/slice/AdaptiveBoxLayoutAbilitySlice.java
@@ -3,12 +3,29 @@ package com.fs.uiapplication.slice;
 import com.fs.uiapplication.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.AdaptiveBoxLayout;
 
 public class AdaptiveBoxLayoutAbilitySlice extends AbilitySlice {
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_adaptivebox_layout);
+
+        AdaptiveBoxLayout adaptiveBoxLayout = (AdaptiveBoxLayout)findComponentById(ResourceTable.Id_adaptive_box_layout);
+
+        findComponentById(ResourceTable.Id_add_rule_btn).setClickedListener((component-> {
+            // Add a rule.
+            adaptiveBoxLayout.addAdaptiveRule(100, 2000, 3);
+            // Update the layout.
+            adaptiveBoxLayout.postLayout();
+        }));
+
+        findComponentById(ResourceTable.Id_remove_rule_btn).setClickedListener((component-> {
+            // Remove the rule.
+            adaptiveBoxLayout.removeAdaptiveRule(100, 2000, 3);
+            // Update the layout.
+            adaptiveBoxLayout.postLayout();
+        }));
     }
 
     @Override
